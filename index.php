5af253e7eb09f957382e6b7b85179784ca26dc3c
Trevor <easyco@gmail.com>
2012-08-18 11:40:12 +1000

    Clean foundation

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>Bear Cal Example</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width">
+
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+    <script src="js/vendor/modernizr-2.6.1.min.js"></script>
+  </head>
+  <body>
+
+    <div class="bearcal">
+    </div>
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.8.0.min.js"><\/script>')</script>
+    <script src="js/plugins.js"></script>
+    <script src="js/bearcal.js"></script>
+    <script src="js/main.js"></script>
+  </body>
+</html>
