1afffa77ec855c4d21868845735a5321d7ccee50
Trevor <easyco@gmail.com>
2012-08-19 17:36:05 +1000

    jQuery Widget Factory boilerplate

--- js/bearcal.js
+++ js/bearcal.js
@@ -1,5 +1,17 @@
 (function() {
 
-
+  (function($, window, document) {
+    return $.widget("ui.BearCal", {
+      options: {
+        period: 12
+      },
+      _create: function() {},
+      destroy: function() {},
+      _setOption: function(key, value) {},
+      getPeriod: function() {
+        return this.options.period;
+      }
+    });
+  })(jQuery, window, document);
 
 }).call(this);
---
+++ js/bearcal.js.coffee
@@ -0,0 +1,15 @@
+(($, window, document) ->
+  $.widget "ui.BearCal",
+  options:
+    period: 12
+
+  _create: ->
+
+  destroy: ->
+
+  _setOption: (key, value) ->
+
+  getPeriod: ->
+    this.options.period
+
+) jQuery, window, document
